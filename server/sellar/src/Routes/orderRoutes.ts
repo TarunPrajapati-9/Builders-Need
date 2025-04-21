ec4cb62fbc14ad6e20c2763e65e4c69f37ac68af
Tarun Prajapati <tarun.prajapati912@gmail.com>
2025-04-22 03:25:17 +0530

    deployment error fixed

--- server/sellar/src/Routes/orderRoutes.ts
+++ server/sellar/src/Routes/orderRoutes.ts
@@ -1,12 +1,12 @@
 import express, { Router } from "express";
-import { getOrders } from "../Controllers/Seller/orders/getOrders";
+import { getSellerOrders } from "../Controllers/Seller/orders/getOrders";
 import { authenticate } from "../Middleware/jwtAuthentication";
 import { updateOrderStatus } from "../Controllers/Seller/orders/updateOrders";
 
 const router: Router = express.Router();
 
 // for seller
-router.get("/orders", authenticate, getOrders);
+router.get("/orders", authenticate, getSellerOrders);
 router.put("/order/:orderId/status", authenticate, updateOrderStatus);
 
 export default router;
